d389a690a6bb113319d1f0363b7f6d917032f3b2
VladMedvedevGit <shomer21@gmail.com>
2024-02-02 18:10:54 +0200

    changed_tel_name

--- front/src/components/PhoneNumbers.vue
+++ front/src/components/PhoneNumbers.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <img alt="Phone icon" src="@/assets/tel.png" width="25px" height="30px" />
+    <img alt="Phone icon" src="../assets/tel.png" width="25px" height="30px" />
     <h2 class="main-color">{{ msg }}</h2>
   </div>
 </template>
