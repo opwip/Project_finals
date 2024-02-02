0908c431d66a4cb3425ed305bec26f1b62298232
opwip <centr.yarik@gmail.com>
2024-02-02 22:02:23 +0200

    Cors???

--- front/src/main.js
+++ front/src/main.js
@@ -11,12 +11,30 @@ import router from "./router";
 import axios from "axios";
 // import VueAwesomeSwiper from "vue-awesome-swiper";
 // Vue.use(VueAwesomeSwiper);
-
+import cors from 'cors';
 
 axios.defaults.baseURL = "https://lionfish-app-n5fr2.ondigitalocean.app";
 
+// Add a request interceptor to include CORS headers
+// axios.interceptors.request.use(function (config) {
+//   config.headers["Access-Control-Allow-Methods"] = "GET,POST";
+//   config.headers["Access-Control-Allow-Origin"] = "*";
+//   config.headers["Access-Control-Allow-Credentials"] = "true";
+//   config.headers["Access-Control-Allow-Headers"] = "Access-Control-Allow-Headers, *,Accept, X-Requested-With, *, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   config.headers["Access-Control-Expose-Headers"] = "*";
+//   return config;
+// });
+// axios.defaults.headers.common = {
+// 'Access-Control-Allow-Methods': 'GET,POST',
+// 'Access-Control-Allow-Origin': '',
+// 'Access-Control-Allow-Credentials': 'true',
+// 'Access-Control-Allow-Headers': '',
+// 'Access-Control-Expose-Headers': '*',
+// }
+// axios.defaults.baseURL = "https://lionfish-app-n5fr2.ondigitalocean.app";
+
 const app = createApp(App);
 
-app.use(router, axios);
+app.use(router, axios, cors());
 
 app.mount("#app");
