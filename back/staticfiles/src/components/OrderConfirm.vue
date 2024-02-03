6e75d318ebd28539f822faec026602d3b5af8a22
opwip <centr.yarik@gmail.com>
2024-02-03 23:09:08 +0200

    cors fix probably

---
+++ back/staticfiles/src/components/OrderConfirm.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div class="wrap">
+    <h1>Дякуємо! Ваше замовлення прийняте</h1>
+    <br />
+    <h4>Наразі воно обробляється, очікуйте листа на вказаий Вами Email</h4>
+    <RouterLink to="/"> <button class="finalBtn">Далі</button></RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  width: 30vw;
+  position: relative;
+}
+h4 {
+  font-weight: bold;
+  font-style: italic;
+}
+.finalBtn {
+  width: 70px;
+  height: 25px;
+  position: absolute;
+  right: 3rem;
+  top: 19rem;
+}
+</style>
---
+++ back/staticfiles/src/components/SearchInput.vue
@@ -0,0 +1,66 @@
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <label for="search">
+    <RouterLink
+      :to="{
+        name: 'SearchView',
+        query: { searchValue: input },
+      }"
+    >
+      <input v-model="input" id="search" />
+      <img
+        alt="Search icon"
+        src="@/assets/search@2x.png"
+        width="26px"
+        height="26px"
+      />
+    </RouterLink>
+  </label>
+</template>
+
+<style scoped>
+input {
+  border: none;
+  font-size: 16px;
+}
+input:active,
+:hover,
+:focus {
+  outline: 0;
+  outline-offset: 0;
+}
+label {
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  height: 2.6rem;
+  width: 15rem;
+  /* margin: 0.2rem 0 0 0.5rem; */
+  display: block;
+  position: relative;
+}
+label img {
+  color: lightgrey;
+  position: absolute;
+  top: 0.5rem;
+  right: 0.8rem;
+  opacity: 0.5;
+}
+
+@media (max-width: 650px) {
+  label img {
+    top: 0.2rem;
+    right: 0.4rem;
+    width: 0.8rem;
+    height: 1rem;
+  }
+}
+</style>
