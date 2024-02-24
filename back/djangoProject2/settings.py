e36d10f3fe9530be537ef44ef2fefdc0d8bbc0d7
opwip <centr.yarik@gmail.com>
2024-02-24 17:59:51 +0200

    Nothing

--- back/djangoProject2/settings.py
+++ back/djangoProject2/settings.py
@@ -161,7 +161,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
